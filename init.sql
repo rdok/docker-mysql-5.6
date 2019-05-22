2ab497edf2021e41a64d69d5cb8197e23cba20fd
Rizart Dokollari <r.dokollari@gmail.com>
2019-05-22 22:49:52 +0100

    Enable persistent database & remove not required local mysql user commands.

--- init.sql
+++ init.sql
@@ -1,5 +1,3 @@
 CREATE USER 'groot'@'%' IDENTIFIED BY 'secret';
-CREATE USER 'groot'@'localhost' IDENTIFIED BY 'secret';
 
 GRANT ALL PRIVILEGES ON *.* TO 'groot'@'%';
-GRANT ALL PRIVILEGES ON *.* TO 'groot'@'localhost';
