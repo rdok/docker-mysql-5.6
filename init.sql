a30acfc91ac2c923da2d41c9c4dcc0839f817b71
Rizart Dokollari <r.dokollari@gmail.com>
2019-05-23 07:31:32 +0100

    Create api & api_test db by default.

--- init.sql
+++ init.sql
@@ -1,3 +1,8 @@
 CREATE USER 'groot'@'%' IDENTIFIED BY 'secret';
 
 GRANT ALL PRIVILEGES ON *.* TO 'groot'@'%';
+
+# If changed: docker-compose down && docker-compose build --no-cache && docker-compose up -d
+CREATE DATABASE api;
+
+CREATE DATABASE api_test;
